b8376fff2be51eeef5716b6067884b9f7bb14db5
DragnGuy <[200~148160995+DragnGuy@users.noreply.github.com>
2024-05-03 09:33:21 -0700

    edited

--- SkyblockProfiles.js
+++ SkyblockProfiles.js
@@ -5,12 +5,12 @@ const profileTableDiv = document.getElementById('Profiletable');
 fetchDataButton.addEventListener('click', () => {
     const playerName = playerNameInput.value;
 
-    fetch(`https://https://api.mojang.com/users/profiles/minecraft/${playerName}`)
+    fetch(`https://api.mojang.com/users/profiles/minecraft/${playerName}`)
     .then(playerNameResponse => playerNameResponse.json())
         .then(IDResult => {
             const PlayerUUID = IDResult.id;
 
-            return fetch(`https://api.hypixel.net/v2/skyblock/profiles?key=d0190f16-1c0e-4953-9f95-924d9c1b8844&uuid=${PlayerUUID}`);
+            return fetch(`https://api.hypixel.net/v2/skyblock/profiles?key=7516f1c9-2688-4183-b377-74aec707a6e5&uuid=${PlayerUUID}`);
         })
         .then(response => response.json())
         .then(data => {
