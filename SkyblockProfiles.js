81b0cd1e0be07289362d40e92c2992314d0c6bee
DragnGuy <4dragon.guy@gmail.com>
2024-05-08 09:00:23 -0700

    removed mojang

--- SkyblockProfiles.js
+++ SkyblockProfiles.js
@@ -8,21 +8,23 @@ fetchDataButton.addEventListener('click', () => {
     fetch(`https://api.hypixel.net/v2/skyblock/profiles?key=${key}&uuid=${PlayerUUID}`)
         .then(res => res.json())
         .then(data => {
+                
             const selectedProfile = data.profiles.find(profile => profile.selected);
-
             if (selectedProfile) {
-                console.log('Selected Profile:', selectedProfile);
-
+                console.log('selected profile found');
             } else {
                 console.error('No selected profile found');
             }
-            })
 
-            .then(
-                fetch(`https://api.hypixel.net/v2/skyblock/profile?key=${key}&profile=${selectedProfile}`))
+            const selectedProfileUUID = data.profiles.selected;
+            
+        
+                fetch(`https://api.hypixel.net/v2/skyblock/profile?key=${key}&profile=${selectedProfileUUID}`)
+    })
                 .then(response => response.json())
                 .then(info => {
                     cutename = info.cute_name;
+                    console.log(cutename)
                 })
 
 
