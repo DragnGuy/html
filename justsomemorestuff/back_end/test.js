42956ad96599b4530187103bb82af5a0e29245ca
DragnGuy <4dragon.guy@gmail.com>
2024-07-11 08:44:57 -0700

    adding to project

---
+++ justsomemorestuff/back_end/test.js
@@ -0,0 +1,85 @@
+const input = `
+☺
+☺♣Count☺
+♥tag    ♦ench☺
+Unbreakable☺♥   HideFlags
+display ♦Lor" 7Gear Score: d1087 8(3097)*7Damage: c+334 e(+20) 8(+1,050.28)27Strength: c+235 e(+20) 9(+50) 8(+750.2)$7Crit Damage: c+70% 8(+238.7%)37Bonus Attack Speed: c+7% 9(+7%) 8(+10.78%)17Intelligence: a+566 9(+125) 8(+1,810.71)▲7Ferocity: a+33 8(+46.2)▲ 8[8✎8] 8[8⚔8]>dldlUltimate Wise V9, 9Cleave V9, 9Critical VI79Cubism V9, 9Ender Slayer V9, 9Experience III?9Fire Aspect II9, 9First Strike III9, 9Giant Killer V89Impaling III9, 9Lethality VI9, 9Life Steal IV/9Looting IV9, 9Luck VI9, 9Prosecute V89Scavenger IV9, 9Sharpness V9, 9Thunderlord VI 9Vampirism V9, 9Venomous V&7Deals +c50% 7damage to Withers.17Grants c+1 c❁ Damage 7and a+2 b✎-bIntelligence 7per cCatacombs 7level.¶aScroll Abilities:,6Ability: Wither Impact  elRIGHT CLICK)7Teleport a10 blocks7 ahead of you.-7Then implode dealing c20,460.6 7damage&7to nearby enemies. Also applies the(7wither shield scroll ability reducing▼7damage taken and granting an)7absorption shield for e5 7seconds.¶8Mana Cost: 3150↕fKills: 61,2619dlkar dldlMYTHIC DUNGEON SWORD dlk♦Name1dHeroic Hyperion 6✪6✪6✪6✪6✪
+ExtraAttributes♥rarity_upgrades☺♥
+stats_book♦♥►hot_potato_count
+upgrade_level☻iHYPERION
+
+enchantmentsimpaling♥♥♦luck♠critical♠♥♠cleave♣♥looting♦♥        scavenger♦♥
+ender_slayer♣♥
+fire_aspect☻♥
+experience♥♥    vampirism♣♥
+life_steal♦♥
+giant_killer♣♥
+first_strike♥venomous♣♥
+ultimate_wise♣♥ sharpness♣♥♠cubism♣♥    lethality♠♥     PROSECUTE♦uuid$f277b5a0-36f7-4372-af01-bcb74eaf8858     ability_scrol♥↕SHADOW_WARP_SCROLL¶WITHER_SHIELD_SCROLL►IMPLOSION_SCROLL♦        timestamp☺ 5☻♠Damage☻☻id☺Z☺♣Count☺
+♥tag    ♦ench☺
+Unbreakable☺♥   HideFlags
+display ♦Lor78Lava Rod↕7Damage: c+100‼7Strength: c+50'7Sea Creature Chance: c+6% 9(+1%) 7Fishing Speed: a+15 9(+2).9Angler V9, 9Blessing IV9, 9Caster V19Frail V9, 9Impaling III9, 9Looting III39Luck of the Sea V9, 9Lure V9, 9Piscary V►9Spiked Hook V"8l* 8Co-op Soulbound 8l*→alUNCOMMON FISHING RO♦Name∟aPitchin' Starter Lava Rod
+ExtraAttributmodifierpitchi☻id►STARTER_LAVA_ROD
+
+enchantments♥♠caster♣impaling♥♥piscary♣♥
+spiked_hook♣♥looting♥♥♣frail♣♥♠angler♣♥luck_of_the_sea♣♥♦lure♣blessing♦uuid$7bea2bc1-2191-4e66-ad59-aae936d0dda2♦       timestamp☺A☻♠Damage☻☻id☺☺♣Count☺
+♥tag    ♦ench
+☺☻♥lvl♣☻☻id ♥   HideFlags
+
+SkullOwne☻Id$4d32846a-7945-345a-8f48-f107f6c92b15
+
+Properties     textures
+♣Value☺ewogICJ0aW1lc3RhbXAiIDogMTYyMDIzOTE2MTcwMywKICAicHJvZmlsZUlkIiA6ICI5ZDIyZGRhOTVmZGI0MjFmOGZhNjAzNTI1YThkZmE4ZCIsCiAgInByb2ZpbGVOYW1lIiA6ICJTYWZlRHJpZnQ0OCIsCiAgInNpZ25hdHVyZVJlcXVpcmVkIiA6IHRydWUsCiAgInRleHR1cmVzIiA6IHsKICAgICJTS0lOIiA6IHsKICAgICAgInVybCIgOiAiaHR0cDovL3RleHR1cmVzLm1pbmVjcmFmdC5uZXQvdGV4dHVyZS83YmYwMWMxOThmNmUxNjk2NWUyMzAyMzVjZDIyYTVhOWY0YTQwZTQwOTQxMjM0NDc4OTQ4ZmY5YTU2ZTUxNzc1IgogICAgfQogIH0KfQ
+display ♦Lor*‼8Breaking Power 9↕7Damage: c+200$7Defense: a+26 9(+16) d(+10) 7Intelligence: a+11 d(+11)#7Mining Speed: a+1,658 d(+58)"7Mining Fortune: a+72 d(+27)▲7Pristine: a+4.6 d(+1.6) 7Mining Wisdom: a+14 9(+5)K 5[a☘5] 5[6⸕5] 5[e✧5] 9[b✎9] 9[5❈9]29Compact IX9, 9Efficiency V9, 9Execute V69Experience III9, 9Fortune IV9, 9Lethality V29Life Steal III9, 9Looting III9, 9Luck V79Pristine III9, 9Scavenger III9, 9Sharpness V‼9Smelting Touch I477Harness the true power of the dGemstones7!.7Each gemstone placed in here will boost its↓7associated astat7.<7eCan be used as both a csword eand a 9pickaxee.§77Your Gemstones:Jade 7(6+27☘7)∟  6Amber 7(6+58⸕7)↔  eTopaz 7(5+1.6✧7)▼  bSapphire 7(b+11✎7)▼  5Amethyst7a20s7.¶8Cooldown: a120s►9Refined Bonus$7Grants 3+5☯ Mining Wisdom7.&7Gain a a0.1% 7chance to drop an#7enchanted item for ores that yo7mine.↑6lLEGENDARY GAUNTLE♦Name∟6Refined Gemstone Gauntlet
+ExtraAttributes
+♦gems
+♠JADE_♦uuid$61eaba66-b981-4b32-ba3d-e8fbc497301qualitFLAWLESS
+AMBER_♦uuid$d75cc329-ba97-4978-8d0c-002bbaa1c2aqualitFLAWLESS
+SAPPHIRE_0♦FINE
+TOPAZ_♦uuid$2802e1f5-440e-449c-a217-e95e70775ecqualitFLAWLESS
+AMETHYST_0♦FImodifierrefined♥compact_blocks     ☻id◄GEMSTONE_GAUNTLET
+
+enchantments♥
+efficiency♣♥fortune♦♥♦luck♣♥smelting_touch☺♥compact     ♥looting♥♥      scavenger♥♥
+experience♥♥
+life_steal♥♥execute♣pristine♥♥  sharpness♣♥     lethality♦uuid$f4f5d147-6075-4d2c-a639-6a6b89cddcfd♦    timestamp☺7@☻♠Damage♥☻☻id☺¶☺♣Count☺
+♥tag    ♦ench☺
+Unbreakable☺♥   HideFlags
+display ♦Lor§↕7Damage: c+100↔7Strength: c+125 9(+25)↨7Crit Damage: c+50%&7Bonus Attack Speed: c+2% 9(+2%) 7Intelligence: a+65 9(+65) 8[7✎8]=dldlUltimate Wise V9, 9Cleave V9, 9Critical V29Cubism V9, 9Ender Slayer V9, 9Execute V>9Experience III9, 9Fire Aspect II9, 9First Strike IV99Giant Killer V9, 9Impaling III9, 9Knockback II79Lethality V9, 9Life Steal III9, 9Looting III19Luck V9, 9Scavenger III9, 9Sharpness V59Thunderlord V9, 9Vampirism V9, 9Venomous V36Ability: Instant Transmission  elRIGHT CLICK,7Teleport a12 blocks7 ahead of you and37gain a+50 f✦ Speed7 for a3 seconds7.‼8Mana Cost: 325►9lRARE SWOR♦NameHeroic Aspect of the End
+ExtraAttributes♥↕tuned_transmissiomodifier♠heroic       originTagQUICK_CRAFTIN☻id◄ASPECT_OF_THE_END
+
+enchantmentsimpaling♥♥♦luck♣critical♣♥♠cleave♣♥looting♥♥
+ender_slayer♣♥  scavenger♥♥     knockback☻♥
+fire_aspect☻♥
+experience♥♥    vampirism♣♥
+life_steal♥♥execute♣♥
+giant_killer♣♥
+first_strike♦venomous♣♥
+ultimate_wise♣♥ sharpness♣♥♠cubism♣♥    lethality♦uuid$38e5a2c8-92a0-41c0-888e-071cf0f352ef♦    timestamp☺t^.2☻♠Damage☻☻id☺♣☺♣Count☺
+♥tag    ♦ench☺
+Unbreakable☺♥   HideFlags
+display ♦Lor↑▼7Gear Score: d685 8(1980)*7Damage: c+364 e(+20) 8(+1,135.53)27Strength: c+89 e(+20) 9(+25) 8(+289.85).7Crit Chance: c+24% 9(+13%) 8(+35.42%)/7Crit Damage: c+171% 9(+50%) 8(+545.6%)↓7Shot Cooldown: a0.5s39Chance IV9, 9Cubism V9, 9Dragon Tracer V89Flame II9, 9Impaling III9, 9Infinite Quiver X-9Piercing I9, 9Power V9, 9Snipe III9Tabasco III↓5◆ Soultwist Rune III▼7Hits c3 7mobs on impact.↨7Can damage endermen.▲6Shortbow: Instantly shoots!►9Precise Bonus$7Deal a+10% 7extra damage when 7arrows hit the head of a mob.:6lkar 6l6lLEGENDARY DUNGEON BOW 6lk♦Name76Precise Juju Shortbow 6✪6✪6✪6✪6✪
+ExtraAttributes♥rarity_upgrades☺♥►hot_potato_count
+
+♣runes♥ SOULTWIST♥☺
+JUJU_SHORTBOW☻idierprecise♥
+
+enchantmentsimpaling♥♥♠chance♦piercing☺♥infinite_quiver
+♥tabasco♥♥♣power♣♥♣snipe♥♥♣flame☻♥♠cubism♣♥♠aiming♦uuid$e224da3c-96d5-40c9-b44d-11a622389751♦   timestamp☺☻♠Damage☻☻id☺♣☺♣Count☺
+♥tag☺
+Unbreakable☺♥   HideFlags
+display ♦Lore
+↕7Damage: c+250¶7Strength: c+100+7Fires a volley of b5 7arrows. Arrows)7apply venom to all enemies hit dealing,7c456❁ Damage 7every second for b3b7seconds.∟4This item is Arachnal Ж! 78This item can be reforged!5lEPIC BO♦Name►5Venom's Touch
+ExtraAttribute☻id
+VENOMS_TOUC♦uuid$e41d4971-a876-46a5-b603-91c29c98d4a2♦  timestamp☺qRTN☻♠Damage☻☻id☺☺♣Count☺
+♥tag♥   HideFlags
+display ♦Lor♣&7View all of your SkyBlock progress,&7including your Skills, Collections,§7Recipes, and more!◄eClick to open♦NameSkyBlock Menu 7(Click)
+SKYBLOCK_MENU☻♠Damage
+`;
+
+
+let findfirstdisplaytag = input.indexOf("display");
+let findfirstExtraAttributestag = input.indexOf("ExtraAttributes");
+let matching = input.substring(findfirstdisplaytag, findfirstExtraAttributestag);
+
+console.log(matching);
